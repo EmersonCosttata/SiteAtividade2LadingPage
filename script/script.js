b8b8cda251f180e1ee00b2126fe89b8e9ae30040
Emerson <euemerson1@gmail.com>
2024-06-10 23:18:42 -0300

    95%

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function addCart(){
+let span = document.getElementById('span')
+    let valorAtual = parseInt(span.innerText)
+    valorAtual++
+    span.innerText = valorAtual
+}
+
+function handleSubmit(){
+    
+
+        alert('Formulário enviado com sucesso! SR: ',nome);
+}
